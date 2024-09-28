0580f867219abc7ca7288dc4fa6d451ed74c51ef
league <league@imac-34.attlocal.net>
2024-09-28 12:58:07 -0700

    start murderofcrows

--- src/_00_Intro_To_ArrayLists/_02_GuestBook.java
+++ src/_00_Intro_To_ArrayLists/_02_GuestBook.java
@@ -1,6 +1,15 @@
 package _00_Intro_To_ArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener {
     /*
      * Create a GUI with two buttons. One button reads "Add Name" and the other
      * button reads "View Names". When the add name button is clicked, display
@@ -13,5 +22,46 @@ public class _02_GuestBook {
      * Guest #3: Greg Ganders
      * Guest #4: Donny Doners
      */
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton("Add Names");
+	JButton button2 = new JButton("View Names");
+	ArrayList<String> names = new ArrayList<String>();
+	
+	public void run() {
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button2);
+		frame.setVisible(true);
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		_02_GuestBook guestBook = new _02_GuestBook();	
+		guestBook.run();
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==button1) {
+			names.add(JOptionPane.showInputDialog("Please enter a name to add to the Guest Book"));
+		}
+		
+		if(e.getSource()==button2) {
+			String list = "";
+			for(int i = 0; i < names.size(); i++) {
+				list+= "Guest #" + (i+1) +": " + names.get(i) + "\n";
+			}
+			JOptionPane.showMessageDialog(null, list);
+		}
+	}
 
 }
